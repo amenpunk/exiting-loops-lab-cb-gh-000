881ef4267b44a6e48b6e6636c1a55830dc5597ab
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-25 20:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ function keepGoing(array,changeValue,skipValue){
   }
   return array;
 }
+
+function findBy(array, findFn){
+   for(let i=0; i<array.length;i++){
+    if(array[i] == findBy)
+    return null;
+  }
+   return array
+}
