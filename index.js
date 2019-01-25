29eca1e655df1ab03d752ec649e3552428a602d5
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-25 20:39:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ function breakOut(array,changeValue,stopValue){
   for(let i = 0; i<array.lenght;i++){
    if(array[i] == stopValue){
     break
-  }else {
+  }
    array[i] = changeValue;
   }
    return array;
 }
+
+function breakOut(array, changeValue, stopValue) {
+  for(let i=0;i<array.length;i++) {
+    if(array[i]==stopValue) {
+      break;
+    }
+    array[i]=changeValue;
+  }
+  return array;
 }
